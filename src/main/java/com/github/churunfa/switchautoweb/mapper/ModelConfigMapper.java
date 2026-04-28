90d8dd52adcdfdce9317a030a68724cba8f6fcbd
churunfa <47979382+churunfa@users.noreply.github.com>
2026-04-28 22:14:50 +0800

    Revert "引入spring ai"
    This reverts commit 89c8abeccdf9648fc59feaaf4b0458c3ff97234f.

--- src/main/java/com/github/churunfa/switchautoweb/mapper/ModelConfigMapper.java
+++
@@ -1,9 +0,0 @@
-package com.github.churunfa.switchautoweb.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.github.churunfa.switchautoweb.entity.ModelConfig;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface ModelConfigMapper extends BaseMapper<ModelConfig> {
-}
